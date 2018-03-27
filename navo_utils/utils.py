5fb3b869c718c368a30fe6364a66598b4fba55e3
Clara Brasseur <cbrasseur@stsci.edu>
2018-03-27 12:26:01 -0400

    moving sia_query to images

--- navo_utils/utils.py
+++ navo_utils/utils.py
@@ -33,10 +33,12 @@ def astropy_table_from_votable_response(response):
     # problems:  It looks for newlines to see if the string is itself a table,
     # and we need to support unicode content.)
     file_like_content = io.BytesIO(response.content)
+
+    # TODO: Add error checking here....
     
     # The astropy table reader will auto-detect that the content is a VOTABLE
     # and parse it appropriately.
-    aptable = Table.read(file_like_content)
+    aptable = Table.read(file_like_content, format='votable')
     
     # String values in the VOTABLE are stored in the astropy Table as bytes instead 
     # of strings.  To makes accessing them more convenient, we will convert all those
@@ -115,45 +117,6 @@ def tap_query(query, tap_service):
     aptable = astropy_table_from_votable_response(results)
     return aptable
 
-def sia_query(access_url, ra, dec, size, image_formats='ALL', print_url=False):
-    """
-        
-    Parameters
-    ----------
-    access_url : str
-        The URL endpoint for the SIA service.
-    ra : str or number
-        The Right Ascension (ICRS degrees) of the central search point
-    dec: str or number
-        The Declination (ICRS degrees) of the central search point
-    size: str or number
-        The diameter of the search circle (cone) in degrees
-    image_formats: str
-        A comma-delimited list of acceptable image MIME types 
-        (choices are usually 'image/fits', 'image/jpeg', 'image/png').
-        May also choose:
-            'ALL' - All formats acceptable (the default)
-            'GRAPHIC' - Allows jpeg png and gif
-            'METADATA' - Denotes a Metadata Query: no images are requested; only metadata 
-            (i.e., SIA result FIELD definitions) should be returned.
-    print_url : bool
-        Whether or not to print the query URL.  
-    
-    Returns
-    -------
-    astropy.table.Table
-        An astropy table created from the contents of the VOTABLE response from the SIA query.
-    """
-    params = {
-        'POS': sval(ra) + ',' + sval(dec),
-        'SIZE': sval(size),
-        'FORMAT': image_formats
-    }
-    results = requests.get(access_url, params)
-    if (print_url):
-        print('Queried: ' + results.url)
-    aptable = astropy_table_from_votable_response(results)
-    return aptable
 
 #
 # Functions to help replace bytes with strings in astropy tables that came from VOTABLEs
