92c94784b8916efa41a629c548df7e0364a7cca5
Basu-Zych <abasuzych@gmail.com>
2018-05-11 12:11:14 -0400

    updated image.py based on Hack day discussion

--- navo_utils/image.py
+++ navo_utils/image.py
@@ -6,9 +6,7 @@ import html # to unescape, which shouldn't be neccessary but currently is
 from . registry import Registry
 from . import utils
 from astropy.table import Table, vstack
-import requests, io, astropy
-
-# Antara: Adding here... 
+import requests, io, astropy 
 import urllib.request
 from astropy.utils.data import download_file
 
@@ -19,7 +17,7 @@ class ImageClass(BaseQuery):
         self._TIMEOUT=3 # seconds to timeout
         self._RETRIES = 3 # total number of times to try
 
-    def query(self, coords, inradius, services=None, max_services=10, **kwargs):
+    def query(self, coords, service, inradius='0.000001'):
         """Basic image search query function 
 
         Input coords should be either a single string, a single
@@ -29,39 +27,19 @@ class ImageClass(BaseQuery):
         string, a single SkyCoord object, or list of 2 as an RA,DEC
         pair.
 
-        Input services can be a string URL, an astropy Table returned
+        Input service can be a string URL, an astropy Table returned
         from Registry.query() (or selected from it), or a single row
         of an astropy Table. If none is given, the kwargs will be
         passed to a Registry.query() call. 
 
         """
-            
-        #Tracer()()
-        # Get the list of URLs that provide matching image searches 
-        #
-        if services is None: 
-            try:
-                services=Registry.query(service_type='image',**kwargs)
-            except:
-                raise 
-        elif type(services) is astropy.table.row.Row:
-            # The user handed one row of an astropy table, e.g., from
-            # a registry query, make it look like a list
-            services=Table(services)
-        elif type(services) is str:
-            services=Table([{'access_url':services}])
-        elif isinstance(services, Table):
-            # In this case, assume they checked the table and query all of them (?)
-            max_services=len(services)
-        else:
-            assert isinstance(services, Table), "ERROR: Don't understand services given; expect a string URL or an astropy Table result from a Registry query."
-
-        assert len(services) <= max_services, "ERROR: You're asking to query more than {} services; max_services is set to {}. If you really want to do more, then set the max_services parameter to a larger number.".format(len(services),max_services)
-            
+        if type(service) is str:
+            service={"access_url":service}
+        
         if type(coords) is str or isinstance(coords,SkyCoord):
             coords=[coords]
         assert type(coords) is list, "ERROR: Give a coordinate object that is a single string, a list/tuple (ra,dec), a SkyCoord, or a list of any of the above."
-
+#        Tracer()()
         if type(inradius) is not list:
             radius =  [inradius]*len(coords)
         else:
@@ -71,7 +49,7 @@ class ImageClass(BaseQuery):
         # Construct list of dictionaries, each with the parameters needed 
         # for the function you're calling in the query_loop:
         params=[{'coords':c,'radius':radius[i]} for i,c in enumerate(coords)] 
-        return utils.query_loop(self._one_image_search, services=services, params=params, max_services=max_services)
+        return utils.query_loop(self._one_image_search, service=service, params=params)
         
     def _one_image_search(self, coords, radius, service):
         if ( type(coords) is tuple or type(coords) is list) and len(coords) == 2:
